fffc8ea457499b1b23366d3317b800a20c737afe
Paul Salcido <paulsalcido.79@gmail.com>
2013-09-21 17:59:06 +0000

    Complete dice project.

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -6,6 +6,14 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class DiceSet
+  attr_accessor :values
+
+  def roll(dice)
+    @values = dice.times.map { |x| 1 + rand(6) }
+  end
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
